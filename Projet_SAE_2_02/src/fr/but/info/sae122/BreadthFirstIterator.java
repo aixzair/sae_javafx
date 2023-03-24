84a1e7743e9a04aa86da8d4dc02988ab4969a2f2
Siopathis Lukas <lukas.siopathis@unicaen.fr>
2023-03-24 15:32:20 +0100

    BFI 0.0.3

--- Projet_SAE_2_02/src/fr/but/info/sae122/BreadthFirstIterator.java
+++ Projet_SAE_2_02/src/fr/but/info/sae122/BreadthFirstIterator.java
@@ -1,25 +1,42 @@
 package src.fr.but.info.sae122;
 
+/*
+ * @author Lukas Siopathis
+ */
+
 import java.util.ArrayList;
 import java.util.Iterator;
 
 public class BreadthFirstIterator implements Iterator<Edge> {
-	private Graph graph;
-	private String currentNode;
-	private ArrayList<String> nodesMet;
-	private Iterator<Edge> itrArr;
-	private int index;
+	private Graph graph;				
+	private String currentNode;			//Noeud actuel
+	private ArrayList<String> nodesMet;	//Liste de noeuds rencontrés
+	private Iterator<Edge> itrArr;		//Itérateur de la liste d'arrêtes
+	private int index;					//Index de la liste de noeuds rencontrés
 
+	/*
+	 * Constructeur d'objets arrêtes
+	 * Le premier noeud devient le noeud actuel
+	 * @param graph : Graphe à parcourir
+	 * @param firstNode : Premier noeud
+	 */
 	public BreadthFirstIterator(Graph graph, String firstNode) {
 		this.graph = graph;
 		this.currentNode = firstNode;
 		this.index = 0;
-		//itrArr = this.graph.
+		nodesMet = new ArrayList<String>();
 		nodesMet.add(firstNode);
 		update();
-		//throw new UnsupportedOperationException("To be written");
 	}
 
+	/*
+	 * Met à jour la liste de Noeuds rencontrés
+	 * @param fromNode : Noeud de départ
+	 * @param toNode : Noeud de destination
+	 * @param label : Etiquette
+	 * @exception : Exception si fromNode et toNode ont la même valeur.
+	 * Renvoie une exception si fromNode == toNode
+	 */
 	public void update()
 	{
 		ArrayList<Edge> al = (ArrayList<Edge>)this.graph.getEdgesFrom(this.currentNode);
@@ -38,19 +55,24 @@ public class BreadthFirstIterator implements Iterator<Edge> {
 		return itrArr.hasNext(); 
 	}
 
+	/*
+	 * @return : La prochaine arrête non-explorée 
+	 */
 	@Override
 	public Edge next() {
 
 		itrArr = this.graph.getEdgesFrom(this.currentNode).iterator();
 			
-		while(!this.hasNext())
+		while(!this.hasNext())		
+			//Incrémente l'index de la liste des noeuds rencontrés si le noeud actuel n'a pas de successeur
 		{
 			this.currentNode = this.nodesMet.get(index);
 			update();
 			index +=1;
 			itrArr = this.graph.getEdgesFrom(this.currentNode).iterator();
 		}
+		Edge arr = this.itrArr.next();
 		this.itrArr.remove();
-		return this.itrArr.next();
+		return arr;
 	}
 }
