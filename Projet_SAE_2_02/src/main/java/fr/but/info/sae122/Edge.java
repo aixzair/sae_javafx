1c7742beda371df7801680085c16afff708619b1
Alenso Lopes <alenso.lopes@gmail.com>
2023-05-16 08:47:53 +0200

    ajout de la capacite et du flux ajout de la capacite et du flux

--- Projet_SAE_2_02/src/main/java/fr/but/info/sae122/Edge.java
+++ Projet_SAE_2_02/src/main/java/fr/but/info/sae122/Edge.java
@@ -6,65 +6,75 @@ import java.util.Objects;
  A simple edge, going from one node to another, with a String label.
  */
 public final class Edge {
-  private final String fromNode;
-  private final String toNode;
-  private final String label;
+    private final String fromNode;
+    private final String toNode;
+    private int capacity;
+    private int flux;
 
-  /** Builds a new edge.
-   * @param fromNode the node to start from
-   * @param toNode the node to arrive at
-   * @param label the label
-   */
-  public Edge(
-      String fromNode,
-      String toNode,
-      String label) {
-    this.fromNode = fromNode;
-    this.toNode = toNode;
-    this.label = label;
-  }
+    /** Builds a new edge.
+     * @param fromNode the node to start from
+     * @param toNode the node to arrive at
+     * @param capacity the label
+     */
+    public Edge(
+            String fromNode,
+            String toNode,
+            int capacity) {
+        this.fromNode = fromNode;
+        this.toNode = toNode;
+        this.capacity = capacity;
+    }
 
-  /** Gets the node this edge starts from
-   * @return the first node
-   */
-  public String getFromNode() {
-    return fromNode;
-  }
+    /** Gets the node this edge starts from
+     * @return the first node
+     */
+    public String getFromNode() {
+        return fromNode;
+    }
 
-  /** Gets the node this edge ends at
-   * @return the last node
-   */
-  public String getToNode() {
-    return toNode;
-  }
+    /** Gets the node this edge ends at
+     * @return the last node
+     */
+    public String getToNode() {
+        return toNode;
+    }
 
-  /** Gets this edge's label
-   * @return the label
-   */
-  public String getLabel() {
-    return label;
-  }
+    /** Gets this edge's label
+     * @return the label
+     */
+    public int getCapacity() {
+        return capacity;
+    }
 
-  @Override
-  public boolean equals(Object obj) {
-    if (obj == this) return true;
-    if (obj == null || obj.getClass() != this.getClass()) return false;
-    var that = (Edge) obj;
-    return Objects.equals(this.fromNode, that.fromNode) &&
-        Objects.equals(this.toNode, that.toNode) &&
-        Objects.equals(this.label, that.label);
-  }
+    public int getFlux() {
+        return flux;
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(fromNode, toNode, label);
-  }
+    public void setFlux(int flux) {
+        this.flux = flux;
+    }
 
-  @Override
-  public String toString() {
-    return "Edge[" +
-        "fromNode=" + fromNode + ", " +
-        "toNode=" + toNode + ", " +
-        "label=" + label + ']';
-  }
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Edge) obj;
+        return Objects.equals(this.fromNode, that.fromNode) &&
+                Objects.equals(this.toNode, that.toNode) &&
+                Objects.equals(this.capacity, that.capacity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fromNode, toNode, capacity);
+    }
+
+    @Override
+    public String toString() {
+        return "Edge[" +
+                "fromNode=" + fromNode + ", " +
+                "toNode=" + toNode + ", " +
+                "Capacity=" + capacity + ", " +
+                "Flux=" + flux  + ']';
+    }
 }
--- Projet_SAE_2_02/src/main/java/fr/but/info/sae122/GraphIO.java
+++ Projet_SAE_2_02/src/main/java/fr/but/info/sae122/GraphIO.java
@@ -5,69 +5,70 @@ import java.util.Scanner;
 
 /** Tool class for reading and writing graphs to/from disk. */
 public class GraphIO {
-  /** Reads a graph from the disk.
-   * @param from the stream to read from
-   * @return the read graph
-   * @throws IOException if something goes wrong with I/O
-   */
-  public static Graph read(InputStream from) throws IOException {
-    var graph = new Graph();
-    BufferedReader reader = new BufferedReader(new InputStreamReader(from));
-    String line;
-    do {
-      line = reader.readLine();
-    } while (line.isBlank());
-    readNodes(graph, line);
-    while (line != null) {
-      do {
-        line = reader.readLine();
-      } while (line != null && line.isBlank());
-      if (line != null) readEdges(graph, line);
-    }
-    return graph;
-  }
-
-  /** Parse a (first) line from the file and adds to the graph.
-   * @param graph the graph to add nodes to
-   * @param line the (nodes) line to parse
-   */
-  private static void readNodes(Graph graph, String line) {
-    var lineScanner = new Scanner(line);
-    lineScanner.useDelimiter(" *; *");
-    while (lineScanner.hasNext()) {
-      graph.addNode(lineScanner.next());
-    }
-  }
-  /** Parse a (not first) line from the file and adds to the graph.
-   * @param graph the graph to add edges to
-   * @param line the (edges) line to parse
-   */
-  private static void readEdges(Graph graph, String line) {
-    var lineScanner = new Scanner(line);
-    lineScanner.useDelimiter(" *; *");
-    var from = lineScanner.next();
-    while (lineScanner.hasNext()) {
-      var to = lineScanner.next();
-      var label = lineScanner.next();
-      graph.addEdge(from,to,label);
-    }
-  }
-
-  /** Writes a graph to the disk.
-   * @param graph the graph to write
-   * @param to the stream to write to
-   */
-  public static void write(Graph graph, OutputStream to) {
-    try (PrintWriter writer = new PrintWriter(to)) {
-      writer.println(String.join(";", graph.getNodes()));
-      for (String node : graph.getNodes()) {
-        writer.printf("%s",node);
-        for (Edge e : graph.getEdgesFrom(node)) {
-          writer.printf(";%s;%s", e.getToNode(), e.getLabel());
+    /** Reads a graph from the disk.
+     * @param from the stream to read from
+     * @return the read graph
+     * @throws IOException if something goes wrong with I/O
+     */
+    public static Graph read(InputStream from) throws IOException {
+        var graph = new Graph();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(from));
+        String line;
+        do {
+            line = reader.readLine();
+        } while (line.isBlank());
+        readNodes(graph, line);
+        while (line != null) {
+            do {
+                line = reader.readLine();
+            } while (line != null && line.isBlank());
+            if (line != null) readEdges(graph, line);
         }
-        writer.println();
-      }
+        return graph;
     }
-  }
+
+    /** Parse a (first) line from the file and adds to the graph.
+     * @param graph the graph to add nodes to
+     * @param line the (nodes) line to parse
+     */
+    private static void readNodes(Graph graph, String line) {
+        var lineScanner = new Scanner(line);
+        lineScanner.useDelimiter(" *; *");
+        while (lineScanner.hasNext()) {
+            graph.addNode(lineScanner.next());
+        }
+    }
+    /** Parse a (not first) line from the file and adds to the graph.
+     * @param graph the graph to add edges to
+     * @param line the (edges) line to parse
+     */
+    private static void readEdges(Graph graph, String line) {
+        var lineScanner = new Scanner(line);
+        lineScanner.useDelimiter(" *; *");
+        var from = lineScanner.next();
+        while (lineScanner.hasNext()) {
+            var to = lineScanner.next();
+            var capacity = lineScanner.nextInt();
+            graph.addEdge(from,to,capacity);
+        }
+    }
+
+    /** Writes a graph to the disk.
+     * @param graph the graph to write
+     * @param to the stream to write to
+     */
+    public static void write(Graph graph, OutputStream to) {
+        try (PrintWriter writer = new PrintWriter(to)) {
+            writer.println(String.join(";", graph.getNodes()));
+            for (String node : graph.getNodes()) {
+                writer.printf("%s",node);
+                for (Edge e : graph.getEdgesFrom(node)) {
+                    writer.printf(";%s;%s", e.getToNode(), e.getCapacity());
+                }
+                writer.println();
+            }
+        }
+    }
+
 
 }
