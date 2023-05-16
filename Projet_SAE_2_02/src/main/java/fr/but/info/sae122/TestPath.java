74b9781a76b89749102483f1cda2e03bd3e6ac65
Siopathis Lukas <lukas.siopathis@unicaen.ffr>
2023-05-16 18:24:27 +0200

    Tentative de correction des tests v4

--- Projet_SAE_2_02/src/main/java/fr/but/info/sae122/TestPath.java
+++ Projet_SAE_2_02/src/main/java/fr/but/info/sae122/TestPath.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class TestPath {
 
 	@Test
-	void testConstructor() {
+	void testConstructor() throws IncoherentSuccessivityException {
 		String node0 = "N0";
 		String node1 = "N1";
 		String node2 = "N2";
@@ -23,40 +23,61 @@ class TestPath {
 		Path path1 = new Path();
 		Path path2;
 		
+		assertNotNull(path1);
+		assertTrue(path1 instanceof Path);
 		
 		assertNotNull(path1);
-		try {
-			path2 = new Path(nodes);//TO DO Erreur
-			assertNotNull(path2);
-		} catch (IncoherentSuccessivityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		path2 = new Path(nodes);//TO DO Erreur
+		assertNotNull(path2);
+		assertTrue(path2 instanceof Path);
 		
 	}
 	
 	@Test
-	void testGetFlow()
+	void testSetGetFlow()
 	{
-		
-	}
-	
-	@Test
-	void testSetFlow()
-	{
-		
+		Path path1 = new Path();
+		assertTrue(path1.getFlow()==0);
+		path1.setFlow(2);
+		assertTrue(path1.getFlow()==2);
 	}
 	
 	@Test
 	void testAddFirstEdge()
 	{
+		String node0 = "N0";
+		String node1 = "N1";
+		String node2 = "N2";
+		
+		Edge edge0 = new Edge(node0, node1, 5);
+		Edge edge1 = new Edge(node1, node2, 5);
+		Edge edge2 = new Edge(node0, node2, 5);
+		
+		Path path1 = new Path();
+		
+		path1.addFirstEdge(edge0);
+		assertTrue(path1.edgeList.get(0) == edge0);
+		
+		path1.addFirstEdge(edge1);
+		assertTrue(path1.edgeList.get(0) == edge1);
 		
 	}
 	
 	@Test
 	void testAddLastEdge()
 	{
+		String node0 = "N0";
+		String node1 = "N1";
+		String node2 = "N2";
 		
+		Edge edge0 = new Edge(node0, node1, 5);
+		Edge edge1 = new Edge(node1, node2, 5);
+		Edge edge2 = new Edge(node0, node2, 5);
+		
+		Path path1 = new Path();
+		
+		path1.addLastEdge(edge0);
+		assertTrue(path1.edgeList.get(path1.edgeList.size()-1) == edge0);
 	}
 
 }
