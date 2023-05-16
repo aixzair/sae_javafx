cd051a91b200ec7dfcce33d375a665597868a2e2
Orianne <orianne.essienth@etu.unicaen.fr>
2023-05-16 09:26:38 +0200

    Path

--- Projet_SAE_2_02/src/main/java/fr/but/info/sae122/Path.java
+++ Projet_SAE_2_02/src/main/java/fr/but/info/sae122/Path.java
@@ -5,17 +5,44 @@ public class Path {
 	Edge[] edgeList;
 	int flow;
 	
+	
 	public Path() {
 		
 	}
 	
-	public Path(String[] nodes) {
+	public Path(String[] nodes, int capacity) throws IncoherentSuccessivityException {
 		
-		for(int i=0;i<nodes.length;i++) {
-			edgeList[i]=new Edge(nodes[i],nodes[i+1],null);
+		for(int i=0;i<nodes.length-1;i++) {
+			edgeList[i]=new Edge(nodes[i],nodes[i+1],capacity);
+		}
+		
+		for(int i=0;i<edgeList.length-1;i++) {
+			
+			if(this.edgeList[i].getToNode()!=this.edgeList[i+1].getFromNode()) {
+				throw new IncoherentSuccessivityException("Deux arêtes successives n'ont pas de lien");
+			}
 		}
 	}
+	
+	public void setFlow(int flow) {
+		this.flow=flow;
+	}
 
+	public int getFlow() {
+		return this.flow;
+	}
+	
+	public void addFirstEdge(Edge edge) {
+		
+		this.edgeList[0]=edge;
+		
+	}
+	
+	public void addLastEdge(Edge edge) {
+		
+		this.edgeList[edgeList.length]=edge;
+	}
+	
 	
 	
 }
