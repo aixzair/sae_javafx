6eb2068d2ae597284559cbb151c30805f48306e5
Siopathis Lukas <lukas.siopathis@unicaen.ffr>
2023-05-16 10:43:34 +0200

    ResidualGraph complet

--- Projet_SAE_2_02/src/main/java/fr/but/info/sae122/ResidualGraph.java
+++ Projet_SAE_2_02/src/main/java/fr/but/info/sae122/ResidualGraph.java
@@ -2,33 +2,34 @@ package main.java.fr.but.info.sae122;
 
 import java.util.List;
 
+/*
+ * Returns a graph of all nodes and not filled edges of a previous graph 
+ */
 public class ResidualGraph{ 
 	
 	Graph createFromGraph(Graph graph)
 	{
 		Graph resGraph = new Graph();
+		/**Fills node list of the residual graph with the original's**/
 		resGraph.nodes.addAll((List<String>) graph.getNodes());
-		//this.edges.addAll((List<Edge>) graph.getEdges());
 		
-		//List<String> nodeList = (List<String>)graph.getNodes();
+		/**Stores edges for ease of use **/
 		List<Edge> edgeList = (List<Edge>)graph.getEdges();
 		for(int i = 0; i < edgeList.size(); i++)
 		{
-			if(edgeList.get(i).getCapacity() - edgeList.get(i).getFlux() >0 )
+			/**Fills the edge list with an edge with no flow if the original one isn't overflowed**/
+			if(edgeList.get(i).getCapacity() - edgeList.get(i).getFlux() > 0 )
 			{
-				Edge newEdge = new Edge(edgeList.get(i).getFromNode(),
-						edgeList.get(i).getToNode(),
-						edgeList.get(i).getCapacity() - edgeList.get(i).getFlux());
-				newEdge.setFlux(0);
-				resGraph.edges.add(newEdge);
+				resGraph.edges.add(new Edge(edgeList.get(i).getFromNode(), 
+						edgeList.get(i).getToNode(), 
+						edgeList.get(i).getCapacity() - edgeList.get(i).getFlux()));
 			}
-			else if(edgeList.get(i).getCapacity() - edgeList.get(i).getFlux() < 0 )
+			/**Fills the edge list with a reversed edge with no flow if the original isn't overflowed and is reduceable**/
+			else if(edgeList.get(i).getFlux() != 0 )
 			{
-				Edge newEdge = new Edge(edgeList.get(i).getToNode(),
-						edgeList.get(i).getFromNode(),
-						edgeList.get(i).getFlux());
-				newEdge.setFlux(0);
-				resGraph.edges.add(newEdge);
+				resGraph.edges.add(new Edge(edgeList.get(i).getToNode(), 
+						edgeList.get(i).getFromNode(),	
+						edgeList.get(i).getFlux()));
 			}
 		}
 		return resGraph;
