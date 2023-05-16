809cf506d00d2fbc5f2f340afe380d5758ec8e51
Orianne <orianne.essienth@etu.unicaen.fr>
2023-05-16 13:54:05 +0200

    AugmentinPath

--- Projet_SAE_2_02/src/main/java/main/java/fr/but/info/sae122/AugmentingPath.java
+++ Projet_SAE_2_02/src/main/java/main/java/fr/but/info/sae122/AugmentingPath.java
@@ -1,21 +1,36 @@
 package main.java.fr.but.info.sae122;
 
-public class AugmentingPath {
+public class AugmentingPath extends Path {
 
 	Graph graph;
 	String sourceNode;
 	String sinkNode;
+	Graph gr;
 	
 	public AugmentingPath(Graph _graph, String _sourceNode, String _sinkNode){
 		
 		this.graph=_graph;
 		this.sourceNode=_sourceNode;
 		this.sinkNode=_sinkNode;
+		BreadthFirstIterator BFI= new BreadthFirstIterator(this.getResidualGraph(),sourceNode);
+		 gr=new Graph();
+		Edge edg = BFI.next().getEdge();
+		while (edg.getToNode()!=sinkNode) {
+			gr.addNode(edg.getFromNode());
+			gr.addNode(edg.getToNode());
+			gr.addEdge(edg.getFromNode(),edg.getToNode(),edg.getCapacity());
+		}
 	}
 	
 	public Graph getResidualGraph() {
 		
+		ResidualGraph graph1 = new ResidualGraph();
+		graph1.createFromGraph(this.graph);
 		
-		return graph; 
+		return graph1; 
+	}
+	
+	public Graph getGr() {
+		return gr;
 	}
 }
