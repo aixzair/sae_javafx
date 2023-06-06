03ed428f6d373b5491f297f8fb2cfa6ad951bfce
Alenso Lopes <alenso.lopes@gmail.com>
2023-06-06 14:21:27 +0200

    ajout du dessin du graph

--- Projet_SAE_2_02/src/main/java/fr/but/info/sae122/seance3/Controller.java
+++ Projet_SAE_2_02/src/main/java/fr/but/info/sae122/seance3/Controller.java
@@ -7,7 +7,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
 
 import java.net.URL;
 import java.util.HashMap;
@@ -18,6 +20,7 @@ public class Controller implements Initializable {
     @FXML
     Canvas canvas;
     @FXML private BorderPane borderPane;
+    @FXML private Pane pane;
 
     private Graph graph;
     private Path path;
@@ -27,20 +30,19 @@ public class Controller implements Initializable {
     public Controller() {
         name = new HashMap<>();
         graph = new Graph();
-        graph.addNode("aa");
-        graph.getNodes().forEach(s -> name.put(s, new GraphicNode(100, 10, 50, Color.RED)));
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        canvas.heightProperty().bind(borderPane.heightProperty());
-        canvas.widthProperty().bind(borderPane.widthProperty());
+        canvas.heightProperty().bind(pane.heightProperty());
+        canvas.widthProperty().bind(pane.widthProperty());
 
         canvas.widthProperty().addListener(observable -> reDraw());
         canvas.heightProperty().addListener(observable -> reDraw());
     }
 
     public void reDraw(){
+        canvas.getGraphicsContext2D().clearRect(0, 0, pane.getHeight(), pane.getWidth());
         for(String s : graph.getNodes()){
             drawNode(s);
         }
@@ -53,27 +55,30 @@ public class Controller implements Initializable {
         double x1 = name.get(s).getX();
         double y1 = name.get(s).getY();
         double radius = name.get(s).getRadius();
+        double width = radius*2;
         Color color = name.get(s).getColor();
 
-        canvas.getGraphicsContext2D().setFill(color);
-        canvas.getGraphicsContext2D().strokeRoundRect(x1, y1, radius, radius, 8, 8);
-        canvas.getGraphicsContext2D().fillRoundRect(x1, y1, radius, radius, 8, 8);
-        canvas.getGraphicsContext2D().strokeText(s, name.get(s).getX() + radius/2-9, name.get(s).getY() + radius/2+5);
+        canvas.getGraphicsContext2D().strokeRoundRect(x1, y1, width, width, radius, radius);
+        canvas.getGraphicsContext2D().setFill(Color.BEIGE);
+        canvas.getGraphicsContext2D().fillRoundRect(x1, y1, width, width, radius, radius);
+        canvas.getGraphicsContext2D().strokeText(s, x1 + radius, y1 + radius);
     }
 
     public void drawEdge(String source, String fin){
-        //save();
+        canvas.getGraphicsContext2D().save();
         double x1 = name.get(source).getX();
         double x2 = name.get(fin).getX();
         double y1 = name.get(source).getY();
         double y2 = name.get(fin).getY();
-        canvas.getGraphicsContext2D().translate(x1, y1);
+        double radius = name.get(source).getRadius();
 
-        double res = Math.toDegrees(Math.atan2(y2-y1, x2-x1));
+        double res = Math.atan2(y2-y1, x2-x1);
+
+        canvas.getGraphicsContext2D().translate(x1, y1);
         canvas.getGraphicsContext2D().rotate(res);
-        canvas.getGraphicsContext2D().beginPath();
-        canvas.getGraphicsContext2D().strokeLine(x1, y1, x2, y2);
-        canvas.getGraphicsContext2D().strokeText(graph.getEdge(source, fin).toString(), x2-x1, y2-y1);
-        //restore();
+
+        canvas.getGraphicsContext2D().strokeLine(0, radius/2,(x2-x1)+10, (y2-y1)+radius/3);
+        canvas.getGraphicsContext2D().strokeText(graph.getEdge(source, fin).getFlow() +  "/" +graph.getEdge(source, fin).getCapacity(), (x2-x1)/2, (y2-y1)/2);
+        canvas.getGraphicsContext2D().restore();
     }
 }
